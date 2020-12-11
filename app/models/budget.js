739df944f64a5b49098767868e0263c6d690318b
jacheampong <jacheampong@gmail.com>
2020-12-11 16:31:15 -0500

    Adding Budget model

---
+++ app/models/budget.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const budgetSchema = Schema({
+    budgetname: { type: String, unique: true, required: true },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    wages: Number,
+    rent: Number,
+    grocery: Number,
+    insurance: Number,
+    phonebill: Number,
+    carpayment: Number,
+    gasoline: Number,
+    others: Number,
+    createdOn: { type: Date, default: Date.now },
+}, {
+    timestamps: true,
+})
+
+const Budget = mongoose.model('Budget', budgetSchema)
+
+module.exports = Budget
