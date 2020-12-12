cf1886913b5502148c7884220a1956b36dce932c
jacheampong <jacheampong@gmail.com>
2020-12-12 14:09:15 -0500

    budget post route setup

--- app/routes/budgets.js
+++ app/routes/budgets.js
@@ -25,6 +25,33 @@ router.get('/api/budgets', (req, res) => {
     });
 });
 
-  
-  // Export the Router so we can use it in the server.js file
-  module.exports = router;
+
+/**
+ * Action:       CREATE
+ * Method:       POST
+ * URI:          /api/budgets
+ * Description:  Create a new Budget
+ */
+router.post('/api/budgets', (req, res) => {
+
+    console.log(req.body)
+    // TODO - update with current user in session
+    currentUser = "5fd438ce9897883dfc205b22"
+    req.body.user = currentUser
+    console.log(req.body)
+    Budget.create(req.body)
+    // On a successful `create` action, respond with 201
+    // HTTP status and the content of the new budget.
+    .then((newBudget) => {
+        res.status(201).json({ budget: newBudget });
+    })
+    // Catch any errors that might occur
+    .catch((error) => {
+        res.status(500).json({ error: error });
+    });
+});
+
+
+
+// Export the Router so we can use it in the server.js file
+module.exports = router;
